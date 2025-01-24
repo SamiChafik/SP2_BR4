0b633d5eee2448cb442d481d28fe7478d0a39cc5
SamiChafik <sami.chafik00@gmail.com>
2025-01-24 17:54:15 +0100

    cleaned the code

--- weather_connect/scripts/script.js
+++ weather_connect/scripts/script.js
@@ -1,37 +1,7 @@
-// const apiKey = "7769113fa81355eb0fa271eba7fc7fa6";
-// const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric";
-
-// document.getElementById("searchButton").addEventListener('click', checkWeather);
-
-//     const city = document.getElementById("search");
-
-// async function checkWeather() {
-//     const response = await fetch(apiUrl + `&q=${city.value}` + `&appid=${apiKey}`);
-//     var data = await response.json();
-//     const icon = document.getElementById("icon");
-
-//     console.log(data);
-    
-//     document.querySelector("#condition").innerHTML = data.weather[0].main;
-//     document.querySelector("#city").innerHTML = data.name;
-//     document.querySelector("#temp").innerHTML = Math.round(data.main.temp) + "°C";
-//     document.querySelector("#humidity").innerHTML = data.main.humidity;
-//     document.querySelector("#wind").innerHTML = `${data.wind.speed}Km/H`;
-
-//     if (data.weather[0].main == 'Clouds') {
-//         icon.src = './assets/icons/animated/cloudy.svg';
-//     }else {icon.src = './assets/icons/animated/day.svg'}
-
-//     checkDay();
-// };
-
 function checkDay() {
     const today = new Date();
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const dayIndex = today.getDay();
-    // const currentDay = days[dayIndex];
-
-    // document.getElementById("currentDay1").innerHTML = currentDay;
 
     for (let i = 0; i <= 4  ; i++) {
         const nextDayIndex = (dayIndex + i) % 7;
@@ -45,8 +15,6 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?units=metric";
 
 document.getElementById("searchButton").addEventListener('click', checkWeather);
 const city = document.getElementById("search");
-// const city = 'marrakesh';
-
 
 async function checkWeather() {
     const response = await fetch(apiUrl + `&q=${city.value}` + `&appid=${apiKey}`);
@@ -55,27 +23,9 @@ async function checkWeather() {
     console.log(data);
     
 
-    document.querySelector("#condition1").innerHTML = data.list[0].weather[0].main;
     document.querySelector("#city").innerHTML = data.city.name;
-    document.querySelector("#temp1").innerHTML = Math.round(data.list[0].main.temp) + "°C";
     document.querySelector("#humidity").innerHTML = data.list[0].main.humidity;
     document.querySelector("#wind").innerHTML = `${data.list[0].wind.speed}km/h`;
-
-    document.querySelector("#temp2").innerHTML = Math.round(data.list[8].main.temp) + "°C";
-    document.querySelector("#condition2").innerHTML = data.list[8].weather[0].main;
-
-    document.querySelector("#temp3").innerHTML = Math.round(data.list[16].main.temp) + "°C";
-    document.querySelector("#condition3").innerHTML = data.list[16].weather[0].main;
-
-    document.querySelector("#temp4").innerHTML = Math.round(data.list[24].main.temp) + "°C";
-    document.querySelector("#condition4").innerHTML = data.list[24].weather[0].main;
-
-    document.querySelector("#temp5").innerHTML = Math.round(data.list[32].main.temp) + "°C";
-    document.querySelector("#condition5").innerHTML = data.list[32].weather[0].main;
-
-    // for (i=0; i<data.list[i+1].length; i++) {
-    //     document.querySelector('#temp'+(i+1)).innerHTML = Math.round(data.list[i+1].main.temp) + "°C";
-    // }
 
     for(let i = 0; i < 5; i++){
         const index = i * 8;
@@ -95,6 +45,10 @@ async function checkWeather() {
             }else if (data.list[index].weather[0].main === 'Thunderstorm') {
                 icon.src = './assets/icons/animated/thunder.svg';
             }  
+
+            document.querySelector(`#temp${i+1}`).innerHTML = Math.round(data.list[index].main.temp) + "°C";
+            document.querySelector(`#condition${i+1}`).innerHTML = data.list[index].weather[0].main;
+        
         }
     }
 
@@ -112,5 +66,3 @@ async function checkWeather() {
     
     checkDay();
 };
-
-
